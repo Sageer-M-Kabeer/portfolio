094aa6b7dce12d2e29d75796ff8c4008554545d7
Sageer-M-Kabeer <sageerm379@gmail.com>
2024-02-29 00:50:08 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
-import './App.css'; // Assuming you have a CSS file named Portfolio.css for the styles
+import './App.css';
+import homeImg from './img/home.jpg'
+import bluesuit from './img/bluesuit.jpg'
+import java from './img/java.png'
+import htm from './img/htm.png'
+import py from './img/py.png'
+import cs from './img/cs.png'
+import jav from './img/jav.png'
+import bash from './img/bash.png'
 
-function App() {
+
+
+
+const App = ()  => {
+
   const [activeLink, setActiveLink] = useState(0);
 
   const handleLinkClick = (index) => {
@@ -22,7 +34,7 @@ function App() {
           <li className={`link ${activeLink === 0 ? 'active' : ''}`} onClick={() => handleLinkClick(0)}><a href="#">home</a></li>
           <li className={`link ${activeLink === 1 ? 'active' : ''}`} onClick={() => handleLinkClick(1)}><a href="#">my projects</a></li>
           <li className={`link ${activeLink === 2 ? 'active' : ''}`} onClick={() => handleLinkClick(2)}><a href="#">about me</a></li>
-          <li className={`link ${activeLink === 3 ? 'active' : ''}`} onClick={() => handleLinkClick(3)}><a href="#">contact us</a></li>
+          <li className={`link ${activeLink === 3 ? 'active' : ''}`} onClick={() => handleLinkClick(3)}><a href="#">contact me</a></li>
         </ul>
       </nav>
 
@@ -30,7 +42,7 @@ function App() {
       <section id="section0" className={`home-section ${activeLink === 0 ? 'active' : ''}`}>
         {/* Home section content */}
         <h1 class="hero-heading">Hello,i am <br/> Sagir</h1>
-        <img src="img/home.jpg" class="home-img" alt=""/>
+        <img src={homeImg} class="home-img" alt=""/>
       </section>
 
       <section id="section1" className={`project-section ${activeLink === 1 ? 'active' : ''}`}>
@@ -38,7 +50,7 @@ function App() {
         <h1 class="project-heading">some of my projects</h1>
     <div class="project-container">
         <div class="project-card">
-            <img src="img/project-1.png" class="project-img" alt=""/>
+            <img src={".img/project-1.png"} class="project-img" alt=""/>
             <div class="project-content">
                 <h1 class="project-title">project 01</h1>
                 <p class="project-info">
@@ -96,7 +108,7 @@ function App() {
         {/* About section content */}
         <div class="about">
         <div class="about-img-container">
-            <img src="img/about me.jpg" class="about-img" alt=""/>
+            <img src={bluesuit} class="about-img" alt=""/>
             <button class="download-cv-btn"></button><a href="">downlaod cv</a>
         </div>
         <p class="about-info">Hi, I am Sagir.Coding has changed my life and my whole world aswell. It's not just about building apps. Learning to code gave meproblem-solving skills and a way to communicate with others on a technical level. I can also develop websites and use my coding skills to get a better job. And I learned it all by myself where I searched and researched on how to code in diffrent languages. I built my own self-esteem and kept myself motivated. My mom is one of the people who belived in me and supports me for coding. I was a beginner and now a pro , so why not you?..
@@ -108,37 +120,37 @@ function App() {
     <h1 class="heading">skills</h1>
     <div class="skills-container">
         <div class="skill-card">
-            <img src="img/htm.png" class="skill-img" alt=""/>
+            <img src={htm} class="skill-img" alt=""/>
             <div class="skill-level">65%</div>
             <h1 class="skill-name">HTML</h1>
             <p class="skill-info">Learned html basics and advanced and obtained 2 certificates</p>
         </div>
         <div class="skill-card">
-            <img src="img/cs.png" class="skill-img" alt=""/>
+            <img src={cs} class="skill-img" alt=""/>
             <div class="skill-level">54%</div>
             <h1 class="skill-name">CSS</h1>
             <p class="skill-info">Learned the basic of css and know how to use it</p>
         </div>
         <div class="skill-card">
-            <img src="img/jav.png" class="skill-img" alt=""/>
+            <img src={jav} class="skill-img" alt=""/>
             <div class="skill-level">43%</div>
             <h1 class="skill-name">JAVASCRIPT</h1>
             <p class="skill-info">Learned about the basics of js and have the basic knowledge</p>
         </div>
         <div class="skill-card">
-            <img src="img/py.png" class="skill-img" alt=""/>
+            <img src={py} class="skill-img" alt=""/>
             <div class="skill-level">76%</div>
             <h1 class="skill-name">PYTHON</h1>
             <p class="skill-info">Knew more about python advanced and core and even GUI !</p>
         </div>
         <div class="skill-card">
-            <img src="img/java.png" class="skill-img" alt=""/>
+            <img src={java} class="skill-img" alt=""/>
             <div class="skill-level">44%</div>
             <h1 class="skill-name">JAVA</h1>
             <p class="skill-info">Knew the basics of java and can build android apps with it</p>
         </div>
         <div class="skill-card">
-            <img src="img/bash.png" class="skill-img" alt=""/>
+            <img src={bash} class="skill-img" alt=""/>
             <div class="skill-level">34%</div>
             <h1 class="skill-name">BASH</h1>
             <p class="skill-info">Knew about the basics of bash scripting and cli commands and earned a certificate</p>
@@ -186,7 +198,7 @@ function App() {
         </form>
         {/* <!-- map --> */}
     <div class="map">
-        <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448181.163742937!2d76.81306771991275!3d28.647279935262464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1639489002410!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+        <iframe title='map' src="" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
     </div>
       </section>
     </div>
